450044f77e759f30b866df1be979d69ab85c9182
timothyas <smith.timothy.andrew@gmail.com>
2025-06-05 13:56:45 -0600

    not the debug queue

--- replay/atmosphere-subsampled/no-log/preprocessing/submit_ufs2arco.sh
+++ replay/atmosphere-subsampled/no-log/preprocessing/submit_ufs2arco.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-#SBATCH -J replay-atm-p0-preprocess
+#SBATCH -J replay-atm-preprocess
 #SBATCH -o preprocessing.%j.out
 #SBATCH -e preprocessing.%j.err
-#SBATCH --nodes=1
+#SBATCH --nodes=4
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=2
-#SBATCH --qos=debug
+#SBATCH --qos=regular
 #SBATCH --account=m4718
 #SBATCH --constraint=cpu
-#SBATCH -t 00:30:00
+#SBATCH -t 02:00:00
 
 module load conda
 conda activate ufs2arco
